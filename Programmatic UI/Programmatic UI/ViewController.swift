b573e2c54f7d782c8850252284f06dd7c4fc7a76
arsartyom <arslanov2004@gmail.com>
2021-02-23 00:44:15 -0800

    The base

---
+++ Programmatic UI/Programmatic UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Programmatic UI
+//
+//  Created by Artem Arslanov on 23.02.2021.
+//  Copyright © 2021 Artem Arslanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
